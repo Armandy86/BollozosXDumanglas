a8425abd9b921d65bfca3793ee8352c49bd11ab4
Armandy86 <armandy.bollozos@urios.edu.ph>
2025-10-22 02:40:50 +0800

    mao

--- public/js/app.js
+++ public/js/app.js
@@ -58272,7 +58272,7 @@ function Courses() {
                 flexShrink: 0,
                 color: '#FFF',
                 fontFamily: 'Poppins, sans-serif',
-                fontSize: '60px',
+                fontSize: '30px',
                 fontStyle: 'italic',
                 fontWeight: 800,
                 lineHeight: '24px',
